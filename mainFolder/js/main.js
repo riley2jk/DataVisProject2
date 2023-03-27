9f9beb623864f16820c8bac5089f61a33d729294
riley2jk <65510000+riley2jk@users.noreply.github.com>
2023-03-26 21:51:14 -0400

    Add Replacement Data file
    Ryan broke it, I fix it..... hopefully

--- mainFolder/js/main.js
+++ mainFolder/js/main.js
@@ -1,15 +1,13 @@
-console.log('Hello World');
+console.log("Hello World");
 
-d3.tsv('data/cincyData.tsv')
-.then(data => {
+d3.tsv("data/cincyData.txt").then((data) => {
     console.log(data[0]);
     console.log(data.length);
-    data.forEach(d => {
-      d.latitude = +d.LATITUDE; //make sure these are not strings
-      d.longitude = +d.LONGITUDE; //make sure these are not strings
+    data.forEach((d) => {
+        d.latitude = +d.LATITUDE; //make sure these are not strings
+        d.longitude = +d.LONGITUDE; //make sure these are not strings
     });
 
     // Initialize chart and then show it
-    leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
-
-  })
+    leafletMap = new LeafletMap({ parentElement: "#my-map" }, data);
+});
