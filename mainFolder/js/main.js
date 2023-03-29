9a2b8de0fd4908d8a14434f06d0b537c13d02a50
ryanrichter <56408485+ryanrichter@users.noreply.github.com>
2023-03-28 20:19:41 -0400

    updated tooltip and data file added colorBy maps

--- mainFolder/js/main.js
+++ mainFolder/js/main.js
@@ -1,7 +1,7 @@
 console.log("Hello World");
 
 d3.tsv("data/cincyData.txt").then((data) => {
-    console.log(data[0]);
+    console.log(data);
     console.log(data.length);
     data.forEach((d) => {
         d.latitude = +d.LATITUDE; //make sure these are not strings
@@ -10,4 +10,27 @@ d3.tsv("data/cincyData.txt").then((data) => {
 
     // Initialize chart and then show it
     leafletMap = new LeafletMap({ parentElement: "#my-map" }, data);
-});
+    leafletMap.updateVis();
+}).catch(error => console.error(error));
+
+
+d3.select('#defaultbutton').on('click', d => {
+    leafletMap.config.colorScaleString = "Default";
+    leafletMap.updateVis();
+  })
+d3.select('#agencybutton').on('click', d => {
+    leafletMap.config.colorScaleString = "byAgency";
+    leafletMap.updateVis();
+  })
+d3.select('#servicebutton').on('click', d => {
+    leafletMap.config.colorScaleString = "byService";
+    leafletMap.updateVis();
+  })
+d3.select('#datebutton').on('click', d => {
+    leafletMap.config.colorScaleString = "byDate";
+    leafletMap.updateVis();
+  })
+d3.select('#monthbutton').on('click', d => {
+    leafletMap.config.colorScaleString = "byMonth";
+    leafletMap.updateVis();
+  })
